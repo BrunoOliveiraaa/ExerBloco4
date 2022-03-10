cf71f3c8ffb4e932aa203680cfee8f79165cc79d
Bruno Oliveira <bruno.dinho.velho@gmail.com>
2022-03-09 22:13:55 -0400

    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,40 @@
+// let Jogador1 = "Papel"
+// let Jogador2 = "Tesoura"
+
+//  if (Jogador1 === 'Pedra' && Jogador2 === 'Papel') {
+//      console.log('Jogador 2 Ganhou!');
+//  } else if (Jogador1 === 'Papel' && Jogador2 === 'Pedra') {
+//      console.log('Jogador 1 Ganhou!');
+//  } 
+//    else if (Jogador1 === 'Papel' && Jogador2 === 'Tesoura') {
+//     console.log('Jogador 2 Ganhou!');
+//  } else if (Jogador1 === 'Tesoura' && Jogador2 === 'Papel') {
+//     console.log('Jogador 1 Ganhou!');
+//  } 
+//    else if (Jogador1 === 'Tesoura' && Jogador2 === 'Pedra') {
+//     console.log('Jogador 2 Ganhou!');
+//  } else if (Jogador1 === 'Pedra' && Jogador2 === 'Tesoura') {
+//     console.log('Jogador 1 Ganhou!');
+//  } 
+//    else if (Jogador1 === 'Papel' && Jogador2 === 'Papel') {
+//    console.log('Empate!');
+//  } else if (Jogador1 === 'Tesoura' && Jogador2 === 'Tesoura') {
+//    console.log('Empate!');
+//  } else if (Jogador1 === 'Pedra' && Jogador2 === 'Pedra') {
+//     console.log('Empate!');
+//  } 
+
+
+
+//  let fruits = [3, 4, 10, 1, 12];
+//  let res = 0;
+//  for(let i = 0; i < fruits.length; i += 1) {
+//      res += fruits[i]
+//    }
+//    if (res > 15){
+//       console.log(res);
+//    } else {
+//       console.log("valor menor que 16");
+//    }
+
+     
